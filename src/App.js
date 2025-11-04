5d531e6313b9386dd9da7576b4a843074e7d26fe
Ramakrishna515 <pramakrishna2002030@gmail.com>
2025-11-04 11:26:20 +0530

    link remove

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          Learn Reacts at <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React official website</a>.
-        </p>
       </header>
     </div>
   );
