b6f4a10791f092c518ed8270d8e7095ac1925632
Ramakrishna515 <pramakrishna2002030@gmail.com>
2025-11-05 16:57:17 +0530

    images add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import './index.css';
 import Button from '@mui/material/Button';
+// import earthImg from "./myimages/earth_9985726.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <header className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white text-center">
+        <img
+          src="/myimages/earth_9985726.png"
+          className="w-40 h-40 animate-spin-slow"
+          alt="Earth"
+        />
+
+        <p className="mt-4">
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <p className="font-bold text-pink-500 text-2xl">R.k Technology...</p>
-        <h1 className="text-3xl font-bold text-green-500 underline mt-4">
+
+        <h1 className="text-3xl font-bold text-green-400 underline mt-4">
           Tailwind CSS is working!
         </h1>
-        <div className='mt-2'>
+
+        <div className="mt-4">
           <Button variant="contained" color="primary">
             Click Me
           </Button>
