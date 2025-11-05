ac862b68b753b9d7959a490e9a919f72a103cc76
Ramakrishna515 <pramakrishna2002030@gmail.com>
2025-11-05 14:45:23 +0530

    material ui configuration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import './index.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
@@ -10,13 +11,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p className='font-bold text-pink-500 text-2xl'>R.k Technology...</p>
+        <p className="font-bold text-pink-500 text-2xl">R.k Technology...</p>
         <h1 className="text-3xl font-bold text-green-500 underline mt-4">
           Tailwind CSS is working!
         </h1>
+        <div className='mt-2'>
+          <Button variant="contained" color="primary">
+            Click Me
+          </Button>
+        </div>
       </header>
     </div>
-
   );
 }
 
