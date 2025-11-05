58b59d104cb5e64e057441a66e4290c36d489260
Ramakrishna515 <pramakrishna2002030@gmail.com>
2025-11-05 12:10:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p className='font-bold text-pink-500 text-2xl'>R.k Technology</p>
+        <p className='font-bold text-pink-500 text-2xl'>R.k Technology...</p>
         <h1 className="text-3xl font-bold text-green-500 underline mt-4">
           Tailwind CSS is working!
         </h1>
